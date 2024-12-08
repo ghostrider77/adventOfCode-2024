f68579cfbb17107286b67f93174957336c447470
Tamas Kurics <tamas.kurics@gmail.com>
2024-12-08 07:05:28 +0100

    day 8
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

---
+++ src/day08b.hs
@@ -0,0 +1,51 @@
+import Control.Monad (guard)
+import Data.List (subsequences)
+import Data.Map (Map, (!))
+import qualified Data.Map as M
+import Data.Set (Set)
+import qualified Data.Set as S
+
+data Coord = Coord Int Int deriving (Eq, Ord, Show)
+data City = City { antennas :: Map Char [Coord], nrRows :: Int, nrCols :: Int } deriving Show
+
+
+parseInput :: [String] -> City
+parseInput [] = error "No input received."
+parseInput ls@(line : _) =
+    let nrCols = length line
+        nrRows = length ls
+        coords = do
+            (row, ix) <- zip ls [0..]
+            (char, jy) <- zip row [0..]
+            guard (char /= '.')
+            return (char, [Coord ix jy])
+    in City { antennas = M.fromListWith (++) coords, nrRows, nrCols}
+
+
+calcAntiNodesOfAFrequency :: City -> [Coord] -> Set Coord
+calcAntiNodesOfAFrequency (City _ nrRows nrCols) locations =
+    let isValidLocation (Coord x y) = 0 <= x && x < nrRows && 0 <= y && y < nrCols
+        calcAntiNodes (Coord x0 y0) (Coord x1 y1) =
+            let dx = x1 - x0
+                dy = y1 - y0
+                up = takeWhile isValidLocation $ map (\k -> Coord (x1 + k * dx) (y1 + k * dy)) [0..]
+                down = takeWhile isValidLocation $ map (\k -> Coord (x0 - k * dx) (y0 - k * dy)) [0..]
+            in S.union (S.fromList up) (S.fromList down)
+        addAntiNodes [c1, c2] = calcAntiNodes c1 c2
+        addAntiNodes _ = S.empty
+        locationPairs = filter ((==2) . length) $ subsequences locations
+    in foldl (\acc pair -> S.union acc $ addAntiNodes pair) S.empty locationPairs
+
+
+getNrAntiNodeLocations :: City -> Int
+getNrAntiNodeLocations city@(City locations _ _ ) =
+    let frequencyLocations = M.elems locations
+        antiNodes = foldl (\acc cs -> S.union acc (calcAntiNodesOfAFrequency city cs)) S.empty frequencyLocations
+    in S.size antiNodes
+
+
+main :: IO ()
+main = do
+    content <- lines <$> getContents
+    let city = parseInput content
+    print $ getNrAntiNodeLocations city
