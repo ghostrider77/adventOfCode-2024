c76b37021ced4216d65982e76e29a01fed0775b0
Tamas Kurics <tamas.kurics@gmail.com>
2024-12-19 14:07:59 +0100

    refactor: use simple count cache instead of the memo trie
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

--- src/day19b.hs
+++ src/day19b.hs
@@ -1,7 +1,7 @@
 import Data.List (stripPrefix)
 import Data.List.Split (splitOn)
 import Data.Maybe (mapMaybe)
-import qualified Data.MemoTrie as M
+import qualified Data.Map as M
 
 
 parseInput :: [String] -> ([String], [String])
@@ -13,8 +13,15 @@ parseInput content =
 
 getNrDifferentArrangements :: String -> [String] -> Int
 getNrDifferentArrangements towel patterns =
-    let arrangements = M.memo (\s -> if null s then 1 else sum (map arrangements (mapMaybe (`stripPrefix` s) patterns)))
-    in arrangements towel
+    let go [] cache = (1, cache)
+        go s cache = case M.lookup s cache of
+            Just count -> (count, cache)
+            Nothing ->
+                let suffixes = mapMaybe (`stripPrefix` s) patterns
+                    (count, cache') =
+                        foldl (\(acc, c) suffix -> let (acc', c') = go suffix c in (acc + acc', c')) (0, cache) suffixes
+                in (count, M.insert s count cache')
+    in fst $ go towel M.empty
 
 
 getTotalNrOfDifferentArrangements :: [String] -> [String] -> Int
